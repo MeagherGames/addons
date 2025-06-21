49e6a088cc05b6e98605e0ffe2c7f53829d6fd6a
Leonard Meagher <leonardmeagher2@gmail.com>
2025-06-21 10:48:32 -0700

    refactor(BackgroundColor2D): simplify color setting logic and remove unused variables

--- addons/BackgroundColor2D/addons/BackgroundColor2D/BackgroundColor2D.gd
+++ addons/BackgroundColor2D/addons/BackgroundColor2D/BackgroundColor2D.gd
@@ -2,39 +2,32 @@
 extends Node2D
 
 const PROJECT_SETTING := "rendering/environment/defaults/default_clear_color"
+const PREV_PROJECT_SETTING := "rendering/environment/defaults/fallback_clear_color"
 
-static var _project_clear_color:Color
-static var _color_is_overridden:bool = false
+@export_color_no_alpha var color: Color = Color.BLACK: set = _set_color
 
-@export_color_no_alpha var color:Color = Color.BLACK : set = _set_color
-
-func _set_color(value:Color) -> void:
-    color = value
-    _unset()
-    if is_visible_in_tree():
-        if Engine.is_editor_hint():
-            if not _color_is_overridden:
-                _project_clear_color = ProjectSettings.get_setting(PROJECT_SETTING, color)
-                _color_is_overridden = true
-            ProjectSettings.set_setting(PROJECT_SETTING, value)
-        RenderingServer.set_default_clear_color(value)
+func _set_color(value: Color) -> void:
+	color = value
+	_unset()
+	if is_visible_in_tree():
+		if not ProjectSettings.has_setting(PREV_PROJECT_SETTING):
+			var current_color = ProjectSettings.get_setting(PROJECT_SETTING, Color.BLACK)
+			ProjectSettings.set_setting(PREV_PROJECT_SETTING, current_color)
+		ProjectSettings.set_setting(PROJECT_SETTING, value)
+		RenderingServer.set_default_clear_color(value)
 
 func _unset() -> void:
-    if Engine.is_editor_hint() and _color_is_overridden:
-        ProjectSettings.set_setting(PROJECT_SETTING, _project_clear_color)
-        _color_is_overridden = false
-        
-    var project_clear_color = ProjectSettings.get_setting(PROJECT_SETTING, color)
-    RenderingServer.set_default_clear_color(project_clear_color)
+	if ProjectSettings.has_setting(PREV_PROJECT_SETTING):
+		var original_color = ProjectSettings.get_setting(PREV_PROJECT_SETTING)
+		ProjectSettings.set_setting(PROJECT_SETTING, original_color)
+		
+	var project_clear_color = ProjectSettings.get_setting(PROJECT_SETTING, color)
+	RenderingServer.set_default_clear_color(project_clear_color)
 
 func _notification(what: int) -> void:
-    match what:
-        NOTIFICATION_ENTER_TREE, NOTIFICATION_ENTER_CANVAS, NOTIFICATION_VISIBILITY_CHANGED, NOTIFICATION_EDITOR_POST_SAVE:
-            if is_visible_in_tree(): _set_color(color)
-            else: _unset()
-        NOTIFICATION_EXIT_TREE, NOTIFICATION_EXIT_CANVAS, NOTIFICATION_EDITOR_PRE_SAVE:
-            _unset()
-        _:
-            pass
-    
-
+	match what:
+		NOTIFICATION_ENTER_TREE, NOTIFICATION_ENTER_CANVAS, NOTIFICATION_VISIBILITY_CHANGED, NOTIFICATION_EDITOR_POST_SAVE:
+			if is_visible_in_tree(): _set_color(color)
+			else: _unset()
+		NOTIFICATION_EXIT_TREE, NOTIFICATION_EXIT_CANVAS, NOTIFICATION_EDITOR_PRE_SAVE:
+			_unset()
