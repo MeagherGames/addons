7492177598386e46c7927fce4b6a29419ec98aa7
Leonard Meagher <LeonardMeagher2@gmail.com>
2025-07-13 09:43:56 -0700

    refactor(Aseprite): remove debug output from execute function

--- addons/AsepriteImporter/addons/aseprite-importer/Aseprite/Aseprite.gd
+++ addons/AsepriteImporter/addons/aseprite-importer/Aseprite/Aseprite.gd
@@ -82,7 +82,6 @@ static func find_aseprite() -> String:
 static func execute(command: String, arguments: Array = [], print_output = false) -> int:
 	var res: int = OK
 	if print_output:
-		print("Executing \"", command, "\" with arguments: ", arguments)
 		var output = []
 		var args = [
 			"--debug", # Enable debug mode
