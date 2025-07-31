378983795bd6af879ad390cc08ef11932b876bc0
DavidMeagher1 <meagherjdavid@gmail.com>
2025-07-30 18:10:55 -0700

    fix(Observers): forgot ObserverNotifier.svg

--- addons/Observers/addons/Observers/observers.gd
+++ addons/Observers/addons/Observers/observers.gd
@@ -7,7 +7,7 @@ func _enter_tree() -> void:
 	ProjectSettings
 	add_custom_type("FrustumShape","Shape3D", preload("res://addons/Observers/FrustumShape.gd"), null)
 	add_custom_type("Observer", "Node3D", preload("res://addons/Observers/Observer.gd"), preload("res://addons/Observers/Observer.svg"))
-	add_custom_type("ObservationNotifier", "Node3D", preload("res://addons/Observers/ObservationNotifier.gd"), null)
+	add_custom_type("ObservationNotifier", "Node3D", preload("res://addons/Observers/ObservationNotifier.gd"), preload("res://addons/Observers/ObservationNotifier.svg"))
 
 func _exit_tree() -> void:
 	# Cleanup of the plugin goes here.
